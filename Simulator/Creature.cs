e63fa9277fbc5c8ed435819b8d7aa1a8ebe26382
SebastianPagacz <234595@student.uek.krakow.pl>
2024-12-02 20:14:19 +0100

    Add project files.

---
+++ Simulator/Creature.cs
@@ -0,0 +1,23 @@
+﻿namespace Simulator;
+
+public class Creature
+{
+    public string name;
+    public int level;
+
+    public Creature(string name, int level)
+    {
+        Name = name;
+        Level = level;
+    }
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+    public int Level
+    {
+        get { return level; }
+        set { level = level > 0 ? level : 1; }
+    }
+}
