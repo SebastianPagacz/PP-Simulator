3271d8af3c420a91abe77d1e855c11e7c4297c4a
SebastianPagacz <234595@student.uek.krakow.pl>
2024-12-10 17:35:32 +0100

    Stwory

--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -5,7 +5,7 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Starting Simulator!\n");
-        Creature elf = new Creature("dupica");
+        Creature elf = new Creature("");
         elf.SayHi();
         Console.WriteLine(elf.Ifno);
     }
