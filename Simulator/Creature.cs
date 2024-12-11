42080a714603deccd7c6fca2b978843581c22a6d
SebastianPagacz <234595@student.uek.krakow.pl>
2024-12-11 01:40:40 +0100

    Settery

--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -2,14 +2,73 @@
 
 internal class Creature
 {
+    // Member variables
+    private string _name = "Unknown";
+    private int _level = 1;
+
+    // Name Lenght variables
+    int shortNameLen = 3;
+    int longNameLen = 25;
+
     // Properties
-    public string Name { get; set; }
-    public int Level { get; set; }
-    public string Ifno 
+    public string Name 
+    {
+        get => _name;
+
+        init
+        {
+            _name = value;
+            while (_name[0] == ' ' || _name[^1] == ' ' || _name.Length < 3 || _name.Length > 25)
+            {
+                // Trimming whitespaces
+                _name = _name.Trim();
+
+                // Adjusting length
+                if (_name.Length < 3)
+                {
+                    while (_name.Length < shortNameLen)
+                    {
+                        _name += "#";
+                    }
+                }
+
+                if(_name.Length > longNameLen)
+                {
+                    int toCut = _name.Length - longNameLen;
+                    _name = _name[..^toCut];
+                }
+            }
+
+            // First liter capital
+            _name = char.ToUpper(_name[0]) + _name.Substring(1);
+        }
+    }
+    public int Level 
+    { 
+        get => _level;
+        init
+        {
+            {
+                if (value < 1)
+                {
+                    _level = 1;
+                }
+                else if (value > 10)
+                {
+                    _level = 10;
+                }
+                else
+                {
+                    _level = value;
+                }
+            }
+        }
+    }
+    public string Info 
     { 
         get { return $"{Name} [{Level}]"; } 
     }
-
+    
     // Constructors
     public Creature(string name, int level = 1)
     {
@@ -21,8 +80,18 @@ internal class Creature
     {
 
     }
+    
+    // Creature methods
     public void SayHi() 
     {
         Console.WriteLine($"Hi, I'm {Name}, my level is {Level}");
+    }
+
+    public void Upgrade()
+    {
+        if(_level < 10)
+        {
+            _level++;
+        }
     }
 }
