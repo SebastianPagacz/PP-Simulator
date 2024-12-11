84fc52392be295480907ddd3ecf89f60fe87c3bb
SebastianPagacz <234595@student.uek.krakow.pl>
2024-12-11 01:40:40 +0100

    Stwory

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -1,11 +1,47 @@
-﻿namespace Simulator;
+﻿using System.Xml.Linq;
+
+namespace Simulator;
 
 internal class Animals
 {
+    int shortNameLen = 3;
+    int longNameLen = 15;
+    private string _description = "Unknown";
     public string Info 
     { 
         get { return $"{Description} <{Size}>"; }
     }
-    public required string Description { get; init; }
+    public required string Description 
+    { 
+        get => _description;
+        init
+        {
+        _description = value;
+        while (_description[0] == ' ' || _description[^1] == ' ' || _description.Length < shortNameLen || _description.Length > longNameLen)
+        {
+            // Trimming whitespaces
+            _description = _description.Trim();
+
+            // Adjusting length
+            if (_description.Length < 3)
+            {
+                while (_description.Length < shortNameLen)
+                {
+                    _description += "#";
+                }
+            }
+
+            if (_description.Length > longNameLen)
+            {
+                int toCut = _description.Length - longNameLen;
+                _description = _description[..^toCut];
+            }
+        }
+
+            // First liter capital
+            _description = char.ToUpper(_description[0]) + _description.Substring(1);
+        }
+    } 
+    
     public uint Size { get; set; } = 3;
 }
--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -5,8 +5,44 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Starting Simulator!\n");
-        Creature elf = new Creature("");
-        elf.SayHi();
-        Console.WriteLine(elf.Ifno);
+
+        // Test #1
+        Lab3a();
+        Console.ReadKey();
+
+        // Test #1 code
+        static void Lab3a()
+        {
+            Creature c = new() { Name = "   Shrek    ", Level = 20 };
+            c.SayHi();
+            c.Upgrade();
+            Console.WriteLine(c.Info);
+
+            c = new("  ", -5);
+            c.SayHi();
+            c.Upgrade();
+            Console.WriteLine(c.Info);
+
+            c = new("  donkey ") { Level = 7 };
+            c.SayHi();
+            c.Upgrade();
+            Console.WriteLine(c.Info);
+
+            c = new("Puss in Boots – a clever and brave cat.");
+            c.SayHi();
+            c.Upgrade();
+            Console.WriteLine(c.Info);
+
+            c = new("a                            troll name", 5);
+            c.SayHi();
+            c.Upgrade();
+            Console.WriteLine(c.Info);
+
+            var a = new Animals() { Description = "   Cats " };
+            Console.WriteLine(a.Info);
+
+            a = new() { Description = "Mice           are great", Size = 40 };
+            Console.WriteLine(a.Info);
+        }
     }
 }
