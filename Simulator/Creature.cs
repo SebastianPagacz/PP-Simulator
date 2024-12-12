53f591d11b33782446d70b248c31b47888b29736
SebastianPagacz <234595@student.uek.krakow.pl>
2024-12-12 16:12:19 +0100

    Elfy i orki

--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -1,6 +1,8 @@
-﻿namespace Simulator;
+﻿using System;
 
-internal class Creature
+namespace Simulator;
+
+internal abstract class Creature
 {
     // Member variables
     private string _name = "Unknown";
@@ -10,8 +12,16 @@ internal class Creature
     int shortNameLen = 3;
     int longNameLen = 25;
 
+
+    //Power for elfs and orcs
+
+    public abstract int Power
+    {
+        get;
+    }
+
     // Properties
-    public string Name 
+    public string Name
     {
         get => _name;
 
@@ -32,7 +42,7 @@ internal class Creature
                     }
                 }
 
-                if(_name.Length > longNameLen)
+                if (_name.Length > longNameLen)
                 {
                     int toCut = _name.Length - longNameLen;
                     _name = _name[..^toCut];
@@ -43,8 +53,8 @@ internal class Creature
             _name = char.ToUpper(_name[0]) + _name.Substring(1);
         }
     }
-    public int Level 
-    { 
+    public int Level
+    {
         get => _level;
         init
         {
@@ -64,32 +74,29 @@ internal class Creature
             }
         }
     }
-    public string Info 
-    { 
-        get { return $"{Name} [{Level}]"; } 
+    public string Info
+    {
+        get { return $"{Name} [{Level}]"; }
     }
-    
+
     // Constructors
-    public Creature(string name, int level = 1)
+    protected Creature(string name, int level = 1)
     {
         Name = name;
         Level = level;
     }
 
-    public Creature()
+    protected Creature()
     {
 
     }
-    
+
     // Creature methods
-    public void SayHi() 
-    {
-        Console.WriteLine($"Hi, I'm {Name}, my level is {Level}");
-    }
+    public abstract void SayHi();
 
     public void Upgrade()
     {
-        if(_level < 10)
+        if (_level < 10)
         {
             _level++;
         }
@@ -106,7 +113,7 @@ internal class Creature
 
     public void Go(Direction[] direction)
     {
-        foreach(var directions in direction)
+        foreach (var directions in direction)
         {
             Go(directions);
         }
@@ -122,4 +129,110 @@ internal class Creature
             Go(dir);
         }
     }
+
+    // Elf and Orc
+    public class Elf : Creature
+    {
+        private int _agility;
+        // Count of sing() usage
+        private int singCount;
+
+        //Power level
+        public override int Power => 8 * Level + 2 * _agility;
+        public int Agility
+        {
+            get => _agility;
+            init
+            {
+                if (value < 0)
+                {
+                    value = 0;
+                    _agility = value;
+                }
+                if (value > 10)
+                {
+                    value = 10;
+                    _agility = value;
+                }
+            }
+        }
+        public void Sing()
+        {
+            singCount++; 
+            Console.WriteLine($"{Name} is singing.");
+
+            if(singCount % 3 == 0)
+            {
+                _agility = Math.Min(_agility + 1, 10);
+            }
+        }
+
+        public Elf() : base() 
+        {
+            _agility = Agility;
+        }
+
+        public Elf(string name, int level = 1, int agility = 1) : base(name, level)
+        {
+            Agility = agility;
+            _agility = agility;
+        }
+
+        public override void SayHi() => Console.WriteLine(
+            $"Hi, I'm {Name}, my level is {Level}, my agility is {Agility}."
+        );
+    }
+
+
+    public class    Orc : Creature
+    {
+        private int _rage;
+        //Hunt count
+        private int rageCount;
+
+        //Power level
+        public override int Power => 7 * Level + 3 * _rage;
+        public int Rage 
+        {  
+            get => _rage;
+            init
+            {
+                if(value < 0)
+                {
+                    value = 0;
+                    _rage = value;
+                }
+                if(value > 10)
+                {
+                    value = 10;
+                    _rage = value;
+                }
+            } 
+        }
+        public void Hunt()
+        {
+            rageCount++;
+            Console.WriteLine($"{Name} is hunting.");
+            if (rageCount % 2 == 0)
+            {
+                _rage = Math.Min(_rage + 1, 10);
+            }
+        }
+
+        public Orc() : base() 
+        {
+            Rage = _rage;
+        }
+
+        public Orc(string name, int level = 1, int rage = 0) : base(name, level)
+        {
+            Rage = rage;
+            _rage = rage;
+        }
+
+        public override void SayHi() => Console.WriteLine(
+            $"Hi, I'm {Name}, my level is {Level}, my rage is {Rage}."
+        );
+    }
+
 }
